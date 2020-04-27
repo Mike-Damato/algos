1dc814994a31cac5b02ee0670e547d229a0077b5
Mike-Damato <damatomike@gmail.com>
2020-04-27 11:20:14 -0400

    Add - 917 Reverse Only Letters

---
+++ LeetCode/917ReverseOnlyLetters.js
@@ -0,0 +1,23 @@
+const reverseOnlyLetters = (S) => {
+  let string = S.split('');
+  let letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let hash = {};
+  for (i = 0; i < letters.length; i++) {
+    let letter = letters[i];
+    hash[letter] = true;
+  }
+  let left = 0;
+  let right = string.length - 1;
+  while (left < right) {
+    if (hash[string[left]] && hash[string[right]]) {
+      [string[left], string[right]] = [string[right], string[left]];
+    }
+    if (!hash[string[left]]) left++;
+    else if (!hash[string[right]]) right--;
+    else {
+      left++;
+      right--;
+    }
+  }
+  return string.join('');
+};
