30112a878b8d36529fd31d468813e194afe85d00
Mike-Damato <damatomike@gmail.com>
2020-05-04 15:06:59 -0400

    Add - Singly Linked List Class

--- LeetCode/Easy/1009ComplementNumbers.js
+++ LeetCode/Easy/1009ComplementNumbers.js
@@ -8,3 +8,18 @@ var findComplement = function (num) {
   }
   return num;
 };
+
+//Alternate solution
+// var bitwiseComplement = function(num) {
+//     if(num === 0){
+//         return 1;
+//     }
+//     let temp = num;
+//     let bit = 1;
+//     while(temp){
+//        num = num ^ bit;
+//         bit = bit << 1;
+//         temp = temp >> 1;
+//     }
+//     return num;
+// };
---
+++ Master Coding Interview/Arrays/SinglyLinkedList.js
@@ -0,0 +1,71 @@
+class Node {
+  constructor(value) {
+    (this.value = value), (this.next = null);
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leaderNode = this.traverseToIndex(index - 1);
+    const holdingPointer = leaderNode.next;
+    leaderNode.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    const leaderNode = this.traverseToIndex(index - 1);
+    const nodeToDelete = leaderNode.next;
+    leaderNode.next = nodeToDelete.next;
+    this.length--;
+    return this.printList();
+  }
+}
