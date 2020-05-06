4dd290eb16051eee276a5ae75d6d0102f371a723
Mike-Damato <damatomike@gmail.com>
2020-05-06 12:23:05 -0400

    Add 169 Majority Element

---
+++ LeetCode/Easy/169MajorityElement.js
@@ -0,0 +1,13 @@
+var majorityElement = function (nums) {
+  let hash = {};
+  for (let number of nums) {
+    if (hash[number]) {
+      hash[number]++;
+    } else {
+      hash[number] = 1;
+    }
+    if (hash[number] > nums.length / 2) {
+      return number;
+    }
+  }
+};
