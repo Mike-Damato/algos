2e3610a68ae9417b3f438b38a9c7cda80a1f00f5
Mike-Damato <damatomike@gmail.com>
2020-05-05 22:15:26 -0400

    Add - 344 Addl Solution

--- LeetCode/Easy/344ReverseString.js
+++ LeetCode/Easy/344ReverseString.js
@@ -1,3 +1,15 @@
 var reverseString = function (s) {
   return s.reverse();
 };
+
+//Swap in place
+var reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    [s[left++], s[right--]] = [s[right], s[left]];
+  }
+
+  return s;
+};
