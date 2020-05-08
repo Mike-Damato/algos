58a8cd92f5cd48e1049895547c91f6f653d4d609
Mike-Damato <damatomike@gmail.com>
2020-05-07 23:14:27 -0400

    Add - 66 Plus One

---
+++ LeetCode/Easy/66PlusOne.js
@@ -0,0 +1,38 @@
+var plusOne = function (digits) {
+  let carry = 0;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (i === digits.length - 1) {
+      if (digits[i] + carry + 1 > 9) {
+        digits[i] = 0;
+        carry = 1;
+      } else {
+        digits[i] += 1;
+        carry = 0;
+        break;
+      }
+    } else {
+      if (digits[i] + carry > 9) {
+        digits[i] = 0;
+        carry = 1;
+      } else {
+        digits[i] += 1;
+        carry = 0;
+        break;
+      }
+    }
+  }
+  if (carry === 1) {
+    digits.unshift(carry);
+  }
+  return digits;
+};
+
+//cleaner
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i] = (digits[i] + 1) % 10;
+    if (digits[i]) return digits;
+  }
+  digits.unshift(1);
+  return digits;
+};
