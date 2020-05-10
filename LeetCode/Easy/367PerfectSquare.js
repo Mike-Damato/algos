0453c5dfe7e01d3cdb807e8291f15bf20d8b494b
Mike-Damato <damatomike@gmail.com>
2020-05-09 21:00:06 -0400

    Add - 367 Perfect Square - Binary Search Solution

--- LeetCode/Easy/367PerfectSquare.js
+++ LeetCode/Easy/367PerfectSquare.js
@@ -10,3 +10,19 @@ var isPerfectSquare = function (num) {
 };
 
 //Binary Search
+var isPerfectSquare = function (num) {
+  let left = 1;
+  let right = num;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let possibleMatch = mid * mid;
+    if (possibleMatch === num) {
+      return true;
+    } else if (possibleMatch > num) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return false;
+};
