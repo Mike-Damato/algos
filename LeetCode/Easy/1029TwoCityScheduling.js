778bdc8f3f3ab650adfcb8a6db6cd497d1f86d9d
Mike-Damato <damatomike@gmail.com>
2020-05-09 20:51:28 -0400

    Add - 367 Perfect Square & 1029 Two City Scheduling

--- LeetCode/Easy/1029TwoCityScheduling.js
+++ LeetCode/Easy/1029TwoCityScheduling.js
@@ -11,3 +11,21 @@ const twoCitySchedCost = (costs) => {
 
   return sum;
 };
+
+//
+
+var twoCitySchedCost = function (costs) {
+  const half = costs.length / 2;
+  let sum = 0;
+
+  let sorted = costs.sort((a, b) => b[1] - b[0] - (a[1] - a[0]));
+
+  for (let i = 0; i < half; i++) {
+    sum += costs[i][0];
+  }
+
+  for (let j = half; j < costs.length; j++) {
+    sum += costs[j][1];
+  }
+  return sum;
+};
---
+++ LeetCode/Easy/367PerfectSquare.js
@@ -0,0 +1,12 @@
+//Brute force
+
+var isPerfectSquare = function (num) {
+  for (let i = 1; i <= num; i++) {
+    if (i * i === num) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//Binary Search
