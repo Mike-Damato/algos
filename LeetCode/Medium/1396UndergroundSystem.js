662bceab3767899440b881b92e032836a60d8298
Mike-Damato <damatomike@gmail.com>
2020-05-11 21:41:07 -0400

    Add - 1396 Design Underground System

---
+++ LeetCode/Medium/1396UndergroundSystem.js
@@ -0,0 +1,32 @@
+var UndergroundSystem = function () {
+  this.checkIns = {};
+  this.trips = {};
+};
+
+UndergroundSystem.prototype.checkIn = function (id, stationName, t) {
+  this.checkIns[id] = { t: t, stationName: stationName };
+};
+
+UndergroundSystem.prototype.checkOut = function (id, stationName, t) {
+  let route = this.checkIns[id].stationName + ' to ' + stationName;
+
+  if (route in this.trips === false) {
+    this.trips[route] = [];
+  }
+  this.trips[route].push(t - this.checkIns[id].t);
+};
+
+UndergroundSystem.prototype.getAverageTime = function (
+  startStation,
+  endStation
+) {
+  //sum of all checkout times - sum of all check in times / number of passengers
+  let route = startStation + ' to ' + endStation;
+  let sum = 0;
+
+  for (let i = 0; i < this.trips[route].length; i++) {
+    sum += this.trips[route][i];
+  }
+
+  return sum / this.trips[route].length;
+};
--- LeetCode/Medium/48RotateImage.js
+++ LeetCode/Medium/48RotateImage.js
@@ -1,12 +1,12 @@
 var rotate = function (matrix) {
   let n = matrix.length;
-  for (let i = 0; i < n; ++i) {
-    for (let j = i; j < n; ++j) {
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
       //es6 swap
       [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
   }
-  for (let k = 0; k < n; ++k) {
+  for (let k = 0; k < n; k++) {
     matrix[k].reverse();
   }
   return matrix;
