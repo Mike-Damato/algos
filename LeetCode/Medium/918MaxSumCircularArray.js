5804df3513d6c34316d5847d34c21e547d234807
Mike-Damato <damatomike@gmail.com>
2020-05-15 16:54:49 -0400

    Add - 918 Add'l answer

--- LeetCode/Medium/918MaxSumCircularArray.js
+++ LeetCode/Medium/918MaxSumCircularArray.js
@@ -16,3 +16,23 @@ var maxSubarraySumCircular = function (A) {
   }
   return Math.max(sum - minOfArr, maxSoFar);
 };
+
+//Alternate
+var maxSubarraySumCircular = function (A) {
+  let maxAtIdx = 0;
+  let maxSoFar = -Infinity;
+  let sum = 0;
+  let currentMin = Infinity;
+  let minOfArr = Infinity;
+  for (let i = 0; i < A.length; i++) {
+    sum += A[i];
+    maxAtIdx = Math.max(A[i], maxAtIdx + A[i]);
+    maxSoFar = Math.max(maxSoFar, maxAtIdx);
+    if (i === A.length - 1) {
+      break;
+    }
+    currentMin = Math.min(A[i], currentMin + A[i]);
+    minOfArr = Math.min(minOfArr, currentMin);
+  }
+  return Math.max(sum - minOfArr, maxSoFar);
+};
