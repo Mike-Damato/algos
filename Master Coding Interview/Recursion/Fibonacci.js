16cb6df2c5169ecee524c892c51b8498baa07937
Mike-Damato <damatomike@gmail.com>
2020-05-21 19:17:05 -0400

    Add - Fibonacci DP solution

--- Master Coding Interview/Recursion/Fibonacci.js
+++ Master Coding Interview/Recursion/Fibonacci.js
@@ -1,18 +1,37 @@
 const fibIterative = (n) => {
   let arr = [0, 1];
   for (let i = 2; i <= n; i++) {
-    arr.push(arr[i - 2] + arr[i - 1])
+    arr.push(arr[i - 2] + arr[i - 1]);
   }
   return arr[n];
-}
-fibIterative(8)
+};
+fibIterative(8);
 
 const fibRecursive = (n) => {
   if (n < 2) {
-    return n
+    return n;
   } else {
-    return fibRecursive(n - 1) + fibRecursive(n - 2)
+    return fibRecursive(n - 1) + fibRecursive(n - 2);
   }
-}
+};
 
-fibRecursive(8)
+fibRecursive(8);
+
+const fibDp = () => {
+  let cache = {};
+  return function fib(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  };
+};
+
+const fastestFib = fibDp;
+fastestFib(8);
