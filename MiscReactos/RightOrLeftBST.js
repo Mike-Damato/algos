717e4450eb8e82157ea8399891836bbdb2009ffb
Mike-Damato <damatomike@gmail.com>
2020-05-26 15:51:56 -0400

    Add - Right or Left BST

---
+++ MiscReactos/RightOrLeftBST.js
@@ -0,0 +1,24 @@
+const solution = (arr) => {
+  // Type your solution here
+  if (arr.length === 0) {
+    return '';
+  }
+  let filtered = arr.filter((elem) => elem !== -1 && elem !== 0);
+  console.log(filtered);
+  let leftSum = 0;
+  let rightSum = 0;
+  for (let i = 1; i < filtered.length; i++) {
+    if (i % 2 !== 0) {
+      leftSum += filtered[i];
+    } else {
+      rightSum += filtered[i];
+    }
+  }
+  if (leftSum > rightSum) {
+    return 'Left';
+  } else if (rightSum > leftSum) {
+    return 'Right';
+  } else {
+    return '';
+  }
+};
