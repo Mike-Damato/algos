7ea307ab88665c575607f0bbcd88b2c8b00607dc
Mike-Damato <damatomike@gmail.com>
2020-05-28 15:33:57 -0400

    Add - Anagrams

---
+++ Aglos & DS Masterclass/Anagrams.js
@@ -0,0 +1,36 @@
+function validAnagram(s1, s2) {
+  // add whatever parameters you deem necessary - good luck!
+  if (s1.length !== s2.length) {
+    return false;
+  }
+  let hash1 = {};
+  let hash2 = {};
+  for (const char of s1) {
+    hash1[char] ? (hash1[char] += 1) : (hash1[char] = 1);
+  }
+  for (const char of s2) {
+    !hash2[char] ? (hash2[char] = 1) : (hash2[char] += 1);
+  }
+  for (const char in hash1) {
+    if (hash1[char] !== hash2[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//Alternate using less space
+function validAnagram(s1, s2) {
+  // add whatever parameters you deem necessary - good luck!
+  if (s1.length !== s2.length) {
+    return false;
+  }
+  let hash = {};
+  for (const char of s1) {
+    hash[char] ? (hash[char] += 1) : (hash[char] = 1);
+  }
+  for (const char of s2) {
+    hash[char] ? (hash[char] -= 1) : false;
+  }
+  return true;
+}
---
+++ AlgoExpert/Hard/FourNumberSum.js
@@ -0,0 +1,27 @@
+function fourNumberSum(arr, target) {
+  // your code goes here
+  let hash = {};
+  let quads = [];
+  let currentSum;
+  let diff;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      currentSum = arr[i] + arr[j];
+      diff = target - currentSum;
+      if (hash[diff]) {
+        for (const pairs of hash[diff]) {
+          quads.push(pairs.concat([arr[i], arr[j]]));
+        }
+      }
+    }
+    for (let k = 0; k < i; k++) {
+      currentSum = arr[i] + arr[k];
+      if (!hash[currentSum]) {
+        hash[currentSum] = [[arr[k], arr[i]]];
+      } else {
+        hash[currentSum].push([arr[k], arr[i]]);
+      }
+    }
+  }
+  return quads;
+}
