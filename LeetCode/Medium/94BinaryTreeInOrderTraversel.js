e32975c339edb2711d8ab3b8e6f19a8eb8a07043
Mike-Damato <damatomike@gmail.com>
2020-06-01 23:32:36 -0400

    Add - Iterative BFS In order solution

--- LeetCode/Medium/94BinaryTreeInOrderTraversel.js
+++ LeetCode/Medium/94BinaryTreeInOrderTraversel.js
@@ -9,3 +9,28 @@ var inorderTraversal = function (root, vals = []) {
   }
   return vals;
 };
+
+//Iterative
+var inorderTraversal = function (root) {
+  const stack = [];
+  const res = [];
+
+  //while there is a root, or there is something in the stack
+  while (root || stack.length) {
+    //if root isn't null / it exists
+    if (root) {
+      //push it into the stack
+      stack.push(root);
+      //reassign, traverse to bottom left
+      root = root.left;
+    } else {
+      //no more left? then start popping off what you have in the stack
+      root = stack.pop();
+      //push vals into res
+      res.push(root.val);
+      //reassign root to be root.right, checks if there's a right
+      root = root.right;
+    }
+  }
+  return res;
+};
