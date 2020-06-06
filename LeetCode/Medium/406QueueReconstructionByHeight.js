2d068768c1d452ef00db25505e1933d63cce4392
Mike-Damato <damatomike@gmail.com>
2020-06-06 12:25:42 -0400

    Update - 406 With additional notes

--- LeetCode/Medium/406QueueReconstructionByHeight.js
+++ LeetCode/Medium/406QueueReconstructionByHeight.js
@@ -1,8 +1,11 @@
 var reconstructQueue = function (people) {
   const compare = ([h1, k1], [h2, k2]) => {
+    //sort by height descending
     if (h1 !== h2) {
       return h2 - h1;
-    } else if (k1 !== k2) {
+    }
+    //then sort by difference in people taller then person ascending
+    else if (k1 !== k2) {
       return k1 - k2;
     }
   };
@@ -10,7 +13,22 @@ var reconstructQueue = function (people) {
   people.sort(compare);
   let ans = [];
   for (const person of people) {
+    //loop thru sorted array then insert appropriate person to appropriate idx
     ans.splice(person[1], 0, person);
   }
   return ans;
 };
+
+/*
+
+example 1:
+
+[[5,0], [5,2], [5,1]] -> [[5,0], [5,1], [5,2]]
+so we sort by height (idx 0) descending,
+then sort by taller or equal height in front (idx 1) ascending
+
+example 2:
+
+[[5,1], [4,1], [5,0]] -> [[5,0], [4,1], [5,1]]
+
+*/
