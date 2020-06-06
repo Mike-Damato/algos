55e7d8c8f1d237e071f0ad37854049bd458a5f6b
Mike-Damato <damatomike@gmail.com>
2020-06-06 15:38:32 -0400

    Update - Update bubblesort with classic optimization

--- Aglos & DS Masterclass/BubbleSort.js
+++ Aglos & DS Masterclass/BubbleSort.js
@@ -8,3 +8,18 @@ const bubbleSort = (arr) => {
   }
   return arr;
 };
+
+//Slight optimization - if data is already sorted we can stop additional passes
+const bubbleSort = (arr) => {
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+};
