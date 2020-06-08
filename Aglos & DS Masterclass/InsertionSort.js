18ab1367f5331eb07b066ff83e8040da7a4f176e
Mike-Damato <damatomike@gmail.com>
2020-06-08 12:12:59 -0400

    Fix - Insertion Sort Algo

--- Aglos & DS Masterclass/InsertionSort.js
+++ Aglos & DS Masterclass/InsertionSort.js
@@ -4,6 +4,7 @@ const insertionSort = (arr) => {
     for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
+    arr[j + 1] = currentVal;
   }
   return arr;
 };
