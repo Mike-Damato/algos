d238821218eb38479531e4485efb9ea7e3132945
Mike-Damato <damatomike@gmail.com>
2020-06-08 12:11:29 -0400

    Add - Insertion Sort

---
+++ Aglos & DS Masterclass/InsertionSort.js
@@ -0,0 +1,9 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+  }
+  return arr;
+};
