c72aa1406581af510f7385ad759a1245a4155a03
Mike-Damato <damatomike@gmail.com>
2020-06-09 11:57:40 -0400

    Add -392 Is Subsequence

--- Aglos & DS Masterclass/Anagrams.js
+++ Aglos & DS Masterclass/Anagrams.js
@@ -9,7 +9,7 @@ function validAnagram(s1, s2) {
     hash1[char] ? (hash1[char] += 1) : (hash1[char] = 1);
   }
   for (const char of s2) {
-    !hash2[char] ? (hash2[char] = 1) : (hash2[char] += 1);
+    hash2[char] ? (hash2[char] += 1) : (hash2[char] = 1);
   }
   for (const char in hash1) {
     if (hash1[char] !== hash2[char]) {
---
+++ LeetCode/Easy/392IsSubsequence.js
@@ -0,0 +1,37 @@
+var isSubsequence = function (s, t) {
+  if (s.length === 0) {
+    return true;
+  }
+  if (t.length === 0) {
+    return false;
+  }
+  let p1 = 0;
+  let p2 = 0;
+  while (p2 <= t.length) {
+    if (p1 === s.length && s[p1] === t[p2]) {
+      return true;
+    } else if (s[p1] === t[p2]) {
+      p1 += 1;
+      p2 += 1;
+    } else {
+      p2 += 1;
+    }
+  }
+  return false;
+};
+
+//Alternate
+var isSubsequence = function (s, t) {
+  let p1 = 0;
+  let p2 = 0;
+  while (p1 < s.length && p2 < t.length) {
+    if (s[p1] == t[p2]) {
+      p1++;
+      p2++;
+    } else {
+      p2++;
+    }
+  }
+
+  return p1 == s.length;
+};
