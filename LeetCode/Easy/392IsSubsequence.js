6a3b1a1c9650dc3ae7ea6ebb2d30c1bea74cbf09
Mike-Damato <damatomike@gmail.com>
2020-06-10 10:10:43 -0400

    Add 355 Search Insert Position & 704 Binary Search

--- LeetCode/Easy/392IsSubsequence.js
+++ LeetCode/Easy/392IsSubsequence.js
@@ -25,7 +25,7 @@ var isSubsequence = function (s, t) {
   let p1 = 0;
   let p2 = 0;
   while (p1 < s.length && p2 < t.length) {
-    if (s[p1] == t[p2]) {
+    if (s[p1] === t[p2]) {
       p1++;
       p2++;
     } else {
@@ -33,5 +33,5 @@ var isSubsequence = function (s, t) {
     }
   }
 
-  return p1 == s.length;
+  return p1 === s.length;
 };
