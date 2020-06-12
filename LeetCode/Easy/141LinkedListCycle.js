1b81e6f716c9ea0e19bea0162c46ca617af657b8
Mike-Damato <damatomike@gmail.com>
2020-06-11 20:01:56 -0400

    Add - 141 alternate solution

--- LeetCode/Easy/141LinkedListCycle.js
+++ LeetCode/Easy/141LinkedListCycle.js
@@ -13,3 +13,17 @@ var hasCycle = function (head) {
   }
   return true;
 };
+
+//Alternate
+var hasCycle = function (head) {
+  let fast = head;
+  let slow = head;
+  while (slow && fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) {
+      return true;
+    }
+  }
+  return false;
+};
