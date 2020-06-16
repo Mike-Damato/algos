d00aeb0a438970c4c65d226771638c21a86c91e6
Mike-Damato <damatomike@gmail.com>
2020-06-16 15:36:56 -0400

    Add - BST Insert Method

--- Aglos & DS Masterclass/BinarySearchTree.js
+++ Aglos & DS Masterclass/BinarySearchTree.js
@@ -10,6 +10,34 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        //returning will break out of while loop
+        //check if val is less or greater than
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
 const myTree = new BinarySearchTree();
