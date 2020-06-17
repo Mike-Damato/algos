d7088bcbefef5aebc2fedfb0d8352ad6e0d4fc9e
Mike-Damato <damatomike@gmail.com>
2020-06-17 12:48:41 -0400

    Add - Three Number Sum AlgoExpert Solution

---
+++ AlgoExpert/Medium/ThreeNumberSum.js
@@ -0,0 +1,24 @@
+const threeNumberSum = (array, targetSum) => {
+  array.sort((a, b) => a - b);
+  let ans = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let currentNum = array[i];
+    let left = i + 1;
+    let right = array.length - 1;
+
+    while (left < right) {
+      let currentSum = currentNum + array[left] + array[right];
+      if (currentSum === targetSum) {
+        ans.push([currentNum, array[left], array[right]]);
+        left++;
+        right--;
+      } else if (currentSum < targetSum) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return ans;
+};
