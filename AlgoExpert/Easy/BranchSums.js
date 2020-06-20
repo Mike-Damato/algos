af5a72e2e3617702e2ca567c9a1a9c178d59f3c2
Mike-Damato <damatomike@gmail.com>
2020-06-20 14:52:58 -0400

    Add - Branch Sums with notes

--- AlgoExpert/Easy/BranchSums.js
+++ AlgoExpert/Easy/BranchSums.js
@@ -7,17 +7,24 @@ class BinaryTree {
 }
 
 const dfs = (node, runningSum, list) => {
+  if (!node) {
+    return null;
+  }
   let newSum = runningSum + node.value;
+  //if it is a leaf node we can push to our list
   if (!node.left && !node.right) {
     list.push(newSum);
-    return;
+    return list;
   }
+  //then we traverse back upwards check if there is a left or a right and repeat
   dfs(node.left, newSum, list);
   dfs(node.right, newSum, list);
 };
 const branchSums = (root) => {
   let arr = [];
-
+  //we start at the root
+  //initial sum is 0 because we havent added anything yet
+  //our arr is our list stores each branch sum
   dfs(root, 0, arr);
   return arr;
 };
