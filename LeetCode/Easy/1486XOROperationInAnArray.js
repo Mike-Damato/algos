b686e4c82449243f10179956e5b94d57e524783e
Mike-Damato <damatomike@gmail.com>
2020-06-21 11:58:51 -0400

    Fix - 1486 XOR Operation In Array & Add - 1487 Making File Names Unique Notes

--- LeetCode/Easy/1486XOROperationInAnArray.js
+++ LeetCode/Easy/1486XOROperationInAnArray.js
@@ -1,6 +1,5 @@
 var xorOperation = function (n, start) {
   let nums = [];
-  let last = nums.length - 1;
   for (let i = 0; i < n; i++) {
     nums.push(start + 2 * i);
   }
--- LeetCode/Medium/1487MakingFileNamesUnique.js
+++ LeetCode/Medium/1487MakingFileNamesUnique.js
@@ -1,11 +1,13 @@
 var getFolderNames = function (names) {
   let hash = new Set();
   for (const name of names) {
+    //If the file name is not in our set we can immediately add it.
     if (!hash.has(name)) {
       hash.add(name);
     } else {
       let k = 1;
       let temp = name;
+      //Otherwise we assign temp to the name then while that "temp" name exists in the set we appeand (k) to the end of it
       while (hash.has(temp)) {
         temp = name + `(${k})`;
         k++;
@@ -13,5 +15,6 @@ var getFolderNames = function (names) {
       hash.add(temp);
     }
   }
+  //Lastly return everything as an array
   return [...hash];
 };
