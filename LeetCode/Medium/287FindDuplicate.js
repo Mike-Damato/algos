22134377cd4d84fb012f08b505f6defc8fe9b10f
Mike-Damato <damatomike@gmail.com>
2020-06-25 10:24:27 -0400

    Add - 287 Find Duplicate Alternate Solution

--- LeetCode/Medium/287FindDuplicate.js
+++ LeetCode/Medium/287FindDuplicate.js
@@ -1,3 +1,19 @@
+//Naive
+var findDuplicate = function (nums) {
+  nums.sort((a, b) => a - b);
+  let p1 = 0;
+  let p2 = 1;
+  while (p2 < nums.length) {
+    if (nums[p1] === nums[p2]) {
+      return nums[p1];
+    } else {
+      p1++;
+      p2++;
+    }
+  }
+};
+
+//Optimal - Floyd's Algo
 var findDuplicate = function (nums) {
   let tortoise = nums[0];
   let hare = nums[0];
