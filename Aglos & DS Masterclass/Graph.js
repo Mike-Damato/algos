ec95a3fec9f14f3bc0f74699e0e826b08d016381
Mike-Damato <damatomike@gmail.com>
2020-06-29 11:24:25 -0400

    Add - Add Edge & Remove Edge Methods for Graph DS

--- Aglos & DS Masterclass/Graph.js
+++ Aglos & DS Masterclass/Graph.js
@@ -7,4 +7,18 @@ class Graph {
       this.adjList[vertex] = [];
     }
   }
+  addEdge(v1, v2) {
+    if (this.adjList.hasOwnProperty(v1) && this.adjList.hasOwnProperty(v2)) {
+      this.adjList[v1].push(v2);
+      this.adjList[v2].push(v1);
+    } else {
+      return undefined;
+    }
+  }
+  removeEdge(v1, v2) {
+    if (this.adjList.hasOwnProperty(v1) && this.adjList.hasOwnProperty(v2)) {
+      this.adjList[v1] = this.adjList[v1].filter((vtx) => vtx !== v2);
+      this.adjList[v2] = this.adjList[v2].filter((vtx) => vtx !== v1);
+    }
+  }
 }
