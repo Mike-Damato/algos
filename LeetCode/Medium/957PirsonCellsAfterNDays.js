4ed1859928395d89d606522371d5f26fddb3fd7c
Mike-Damato <damatomike@gmail.com>
2020-07-03 12:48:41 -0400

    Add - 957 Alt Solution

--- LeetCode/Medium/957PirsonCellsAfterNDays.js
+++ LeetCode/Medium/957PirsonCellsAfterNDays.js
@@ -20,3 +20,32 @@ var prisonAfterNDays = function (cells, N) {
   }
   return cells;
 };
+
+//alternate
+const prisonAfterNDays = (cells, N) => {
+  if (N % 14 === 0) {
+    N = 14;
+  } else {
+    N = N % 14;
+  }
+  let myMap = new Map();
+  for (let i = 0; i < N; i++) {
+    let arr = [];
+    arr[0] = 0;
+    arr[7] = 0;
+    if (myMap.has(cells)) {
+      cells = myMap.get(cells);
+      continue;
+    }
+    for (let j = 1; j < 7; j++) {
+      if (cells[j - 1] === cells[j + 1]) {
+        arr[j] = 1;
+      } else {
+        arr[j] = 0;
+      }
+    }
+    myMap.set(cells, arr);
+    cells = arr;
+  }
+  return cells;
+};
