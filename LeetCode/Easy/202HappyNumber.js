b0763eb4dee518d1b450bcf67e7746728592a6b6
Mike-Damato <damatomike@gmail.com>
2020-07-03 12:16:16 -0400

    Add - 202 Happy Number

---
+++ LeetCode/Easy/202HappyNumber.js
@@ -0,0 +1,20 @@
+var isHappy = function (n, count = 0) {
+  let ans = false;
+  if (count < 10) {
+    //split number and then make a new array squared, then add them with reduce
+    let sum = n
+      .toString()
+      .split('')
+      .map((n) => n * n)
+      .reduce((a, b) => a + b);
+
+    if (sum === 1) {
+      ans = true;
+      // return ans;
+    } else {
+      count++;
+      return isHappy(sum, count);
+    }
+  }
+  return ans;
+};
