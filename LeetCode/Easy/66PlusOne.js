f0637a9e74dc8961482ff76a1a5bffa6edfa85c7
Mike-Damato <damatomike@gmail.com>
2020-07-06 10:42:49 -0400

    Add - Alternate Solution to plus one

--- LeetCode/Easy/66PlusOne.js
+++ LeetCode/Easy/66PlusOne.js
@@ -36,3 +36,16 @@ var plusOne = function (digits) {
   digits.unshift(1);
   return digits;
 };
+
+//Alt solution again
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      return digits;
+    }
+  }
+  return [1].concat(digits);
+};
