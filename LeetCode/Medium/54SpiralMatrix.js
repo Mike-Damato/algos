bc34ae0d70e12f74f9bc2b5b06f0251d25086e82
Mike-Damato <damatomike@gmail.com>
2020-07-07 13:18:20 -0400

    Add - 54 Spiral Matrix Alt Solution

--- LeetCode/Medium/54SpiralMatrix.js
+++ LeetCode/Medium/54SpiralMatrix.js
@@ -34,3 +34,46 @@ var spiralOrder = function (matrix) {
   }
   return result;
 };
+
+//Alternate
+var spiralOrder = function (array) {
+  let ans = [];
+
+  if (array.length < 1) {
+    return ans;
+  }
+
+  let startRow = 0;
+  let endRow = array.length - 1;
+  let startCol = 0;
+  let endCol = array[0].length - 1;
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let col = startCol; col <= endCol; col++) {
+      ans.push(array[startRow][col]);
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++) {
+      ans.push(array[row][endCol]);
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--) {
+      if (startRow === endRow) {
+        break;
+      }
+      ans.push(array[endRow][col]);
+    }
+
+    for (let row = endRow - 1; row > startRow; row--) {
+      if (startCol === endCol) {
+        break;
+      }
+      ans.push(array[row][startCol]);
+    }
+    startRow++;
+    endRow--;
+    startCol++;
+    endCol--;
+  }
+  return ans;
+};
