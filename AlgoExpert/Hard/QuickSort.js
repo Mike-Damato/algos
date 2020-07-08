3562f1abfdf69c93d97015ae59df8fc3dca8b897
Mike-Damato <damatomike@gmail.com>
2020-07-08 14:30:01 -0400

    Add - QuickSort

---
+++ AlgoExpert/Hard/QuickSort.js
@@ -0,0 +1,25 @@
+function quickSort(array, left = 0, right = array.length - 1) {
+  // Write your code here.
+  if (left < right) {
+    let pivotIdx = pivotHelper(array, left, right);
+    quickSort(array, left, pivotIdx - 1);
+    quickSort(array, pivotIdx + 1, right);
+  }
+  return array;
+}
+
+const pivotHelper = (array, start = 0, end = array.length - 1) => {
+  let pivot = array[start];
+  let swapIdx = start;
+  for (let i = start + 1; i < array.length; i++) {
+    if (pivot > array[i]) {
+      swapIdx++;
+      [array[i], array[swapIdx]] = [array[swapIdx], array[i]];
+    }
+  }
+  [array[start], array[swapIdx]] = [array[swapIdx], array[start]];
+  return swapIdx;
+};
+
+// Do not edit the line below.
+exports.quickSort = quickSort;
