286923b4823b33a220d1263820fa023be1a78825
Mike-Damato <damatomike@gmail.com>
2020-07-11 11:18:30 -0400

    Add -  5177 Reformat Date

---
+++ LeetCode/Easy/5177ReformatDate.js
@@ -0,0 +1,46 @@
+var reformatDate = function (date) {
+  let hash = {
+    Jan: '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12',
+    s: true,
+    t: true,
+    n: true,
+    d: true,
+    r: true,
+    h: true,
+  };
+
+  let endIdx;
+  for (let i = 0; i < date.split('').length; i++) {
+    if (hash.hasOwnProperty(date[i])) {
+      endIdx = i;
+      break;
+    }
+  }
+
+  let newDay = '';
+  for (let i = 0; i < endIdx; i++) {
+    let char = date[i];
+    newDay += char;
+  }
+
+  if (newDay.length === 1) {
+    let lastNum = newDay[newDay.length - 1];
+    newDay = 0 + lastNum;
+  }
+
+  let month = date.slice(-8, -5);
+  let newDate = date.slice(-4) + '-' + hash[month] + '-' + newDay;
+
+  return newDate;
+};
