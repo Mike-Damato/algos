0d83a37e37cbdd47a01606f8963753f512293e0c
Mike-Damato <damatomike@gmail.com>
2020-07-14 15:31:04 -0400

    Add - 189 Rotate Array Alt Solution

--- LeetCode/Easy/189RotateAnArray.js
+++ LeetCode/Easy/189RotateAnArray.js
@@ -1,3 +1,15 @@
 var rotate = function (nums, k) {
   return nums.unshift(...nums.splice(nums.length - k));
 };
+
+//Alternate
+var rotate = function (nums, k) {
+  let count = 0;
+  let newNum;
+  while (count < k) {
+    newNum = nums.pop();
+    nums.unshift(newNum);
+    count++;
+  }
+  return nums;
+};
