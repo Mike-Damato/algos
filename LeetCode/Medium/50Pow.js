85f3bab40c5d75835425ccc5dbee12be632e2547
Mike-Damato <damatomike@gmail.com>
2020-07-16 10:12:18 -0400

    Add - 50 Pow Alt Solution

--- LeetCode/Medium/50Pow.js
+++ LeetCode/Medium/50Pow.js
@@ -1,3 +1,24 @@
 var myPow = function (x, n) {
   return Math.pow(x, n);
 };
+
+var myPow = function (x, n) {
+  if (n === 0) {
+    return 1;
+  }
+
+  let newExp = Math.abs(n);
+
+  let ans;
+  if (newExp % 2 === 0) {
+    ans = myPow(x * x, newExp / 2);
+  } else {
+    ans = myPow(x * x, (newExp - 1) / 2) * x;
+  }
+
+  if (n < 0) {
+    return 1 / ans;
+  } else {
+    return ans;
+  }
+};
