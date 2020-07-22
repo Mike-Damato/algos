79415d6300b0ff27e81fbbf14ed4ef0379cbeb37
Mike-Damato <damatomike@gmail.com>
2020-07-22 12:13:17 -0400

    Add - LRU Cache

---
+++ AlgoExpert/Very Hard/LRUCache.js
@@ -0,0 +1,119 @@
+class LRUCache {
+  constructor(maxSize) {
+    this.cache = {};
+    this.maxSize = maxSize || 1;
+    this.currentSize = 0;
+    this.listOfMostRecent = new DLL();
+  }
+
+  insertKeyValuePair(key, value) {
+    // Write your code here.
+    if (!this.cache.hasOwnProperty(key)) {
+      if (this.currentSize === this.maxSize) {
+        this.evictLeastRecent();
+      } else {
+        this.currentSize += 1;
+      }
+      this.cache[key] = new DLLNode(key, value);
+    } else {
+      this.replaceKey(key, value);
+    }
+    this.updateMostRecent(this.cache[key]);
+  }
+
+  getValueFromKey(key) {
+    // Write your code here
+    if (!this.cache.hasOwnProperty(key)) {
+      return null;
+    }
+    // console.log(this.cache[key])
+
+    this.updateMostRecent(this.cache[key]);
+    return this.cache[key].value;
+  }
+
+  getMostRecentKey() {
+    // Write your code here.
+    if (!this.listOfMostRecent.head) {
+      return;
+    }
+    return this.listOfMostRecent.head.key;
+  }
+
+  replaceKey(key, value) {
+    if (!this.cache.hasOwnProperty(key)) {
+      throw new Error('Key not found!');
+    }
+    this.cache[key].value = value;
+  }
+
+  evictLeastRecent() {
+    const keyToDelete = this.listOfMostRecent.tail.key;
+    this.listOfMostRecent.removeTail();
+    delete this.cache[keyToDelete];
+  }
+
+  updateMostRecent(node) {
+    this.listOfMostRecent.setHeadTo(node);
+  }
+}
+
+class DLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  setHeadTo(node) {
+    if (this.head === node) {
+      return;
+    } else if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else if (this.head === this.tail) {
+      this.tail.prev = node;
+      this.head = node;
+      this.head.next = this.tail;
+    } else {
+      if (this.tail === node) {
+        this.removeTail();
+      }
+      node.removeBindings();
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+  }
+
+  removeTail() {
+    if (this.tail === null) {
+      return;
+    }
+    if (this.tail === this.head) {
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+  }
+}
+
+class DLLNode {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+  removeBindings() {
+    if (this.prev !== null) {
+      this.prev.next = this.next;
+    }
+    if (this.next !== null) {
+      this.next.prev = this.prev;
+    }
+    this.prev = null;
+    this.next = null;
+  }
+}
