f06a4e7869517beefb38b15863feb9064ee4e809
Mike-Damato <damatomike@gmail.com>
2020-07-23 15:50:20 -0400

    Add - 695 Max Area of Island

---
+++ LeetCode/Medium/695MaxAreaOfIsland.js
@@ -0,0 +1,35 @@
+var maxAreaOfIsland = function (grid) {
+  let area = 0;
+  let maxArea = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        area = dfs(grid, i, j);
+        maxArea = Math.max(area, maxArea);
+      }
+    }
+  }
+  return maxArea;
+};
+
+const dfs = (grid, i, j, area = 1) => {
+  if (
+    i < 0 ||
+    i >= grid.length ||
+    j < 0 ||
+    j >= grid[i].length ||
+    grid[i][j] === 0
+  ) {
+    return 0;
+  }
+  // let area = 1;
+  grid[i][j] = 0;
+
+  area += dfs(grid, i - 1, j);
+  area += dfs(grid, i + 1, j);
+  area += dfs(grid, i, j - 1);
+  area += dfs(grid, i, j + 1);
+
+  // console.log(area)
+  return area;
+};
