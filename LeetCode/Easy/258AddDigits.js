3e0e0564fdb9bb93318b2bbc7e31490eb7891f37
Mike-Damato <damatomike@gmail.com>
2020-07-26 11:44:55 -0400

    Add  - 258 Alternate Recursive Solution

--- LeetCode/Easy/258AddDigits.js
+++ LeetCode/Easy/258AddDigits.js
@@ -13,3 +13,20 @@ var addDigits = function (num) {
   }
   return num;
 };
+
+//Recursive
+ * /
+var addDigits = function (num) {
+  if (num <= 9) {
+    return num;
+  } else {
+    let newNum = 0;
+    while (num > 0) {
+      let lastDigit = Math.floor(num % 10);
+      newNum += lastDigit
+      num /= 10;
+    }
+    return addDigits(newNum)
+  }
+
+};
