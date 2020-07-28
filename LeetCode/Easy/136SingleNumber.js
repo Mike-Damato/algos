76b75943ee0d39a89aae161f4814080a4227d93e
Mike-Damato <damatomike@gmail.com>
2020-07-28 11:43:40 -0400

    Add - 136 Solutions using Map & Set Datastructures

--- LeetCode/Easy/136SingleNumber.js
+++ LeetCode/Easy/136SingleNumber.js
@@ -13,3 +13,29 @@ var singleNumber = function (nums) {
     }
   }
 };
+
+//Using a map
+var singleNumber = function (nums) {
+  let map = new Map();
+  for (const num of nums) {
+    if (map.has(num)) {
+      map.delete(num);
+    } else {
+      map.set(num, 1);
+    }
+  }
+  return map.keys().next().value;
+};
+
+//Using a set
+var singleNumber = function (nums) {
+  let set = new Set();
+  for (const num of nums) {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+  return set.values().next().value;
+};
