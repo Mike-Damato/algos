dfdd3a9c803dc39c1c9475920163043e991c5ee0
Mike-Damato <damatomike@gmail.com>
2020-07-31 21:31:47 -0400

    Fix - 139 Word Break

--- LeetCode/Medium/139WordBreak.js
+++ LeetCode/Medium/139WordBreak.js
@@ -4,9 +4,8 @@ const wordBreak = (s, wordDict) => {
     set.add(word);
   }
 
-  // let length = s.length;
   let dp = new Array(s.length + 1).fill(false);
-  dp[0] = 0;
+  dp[0] = true;
   for (let i = 1; i <= s.length; i++) {
     for (let j = 0; j < i; j++) {
       if (dp[j] && set.has(s.substring(j, i))) {
