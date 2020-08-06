79b45890c17f706211d240b68d152de1eaae9f8a
Mike-Damato <damatomike@gmail.com>
2020-08-06 17:36:52 -0400

    Add - 214 Shortest Palindrome

--- LeetCode/Hard/214ShortestPalindrome.js
+++ LeetCode/Hard/214ShortestPalindrome.js
@@ -12,3 +12,26 @@ var shortestPalindrome = function (s) {
   }
   return reverse.substr(0, idx) + str;
 };
+
+//Alternate Using KMP
+const shortestPalindrome = (s) => {
+  let reversed = s.split('').reverse().join('');
+  let current = s + '#' + reversed;
+  let length = current.length;
+  let prefix = new Array(length).fill(0);
+  let i = 1;
+  let j = 0;
+  while (i < length) {
+    if (current[i] === current[j]) {
+      prefix[i] = j + 1;
+      i++;
+      j++;
+    } else if (j > 0) {
+      j = prefix[j - 1];
+    } else {
+      i++;
+    }
+  }
+  let temp = reversed.substr(0, s.length - prefix[length - 1]);
+  return temp + s;
+};
