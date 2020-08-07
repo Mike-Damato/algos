112076673571042f4a190c2b458de932769064a8
Mike-Damato <damatomike@gmail.com>
2020-08-07 11:38:15 -0400

    Add - Find K Closest Elements

---
+++ LeetCode/Medium/658FindKClosestElements.js
@@ -0,0 +1,16 @@
+var findClosestElements = function (arr, k, x) {
+  let left = 0;
+  let right = arr.length - k;
+  let mid;
+
+  while (left < right) {
+    mid = left + Math.floor((right - left) / 2);
+
+    if (x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return arr.slice(left, left + k);
+};
