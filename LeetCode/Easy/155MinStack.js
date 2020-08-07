e61a0155eb554fc7f097c96bb143aaaee9eb06e5
Mike-Damato <damatomike@gmail.com>
2020-08-07 11:48:23 -0400

    Add - 155 Min Stack

---
+++ LeetCode/Easy/155MinStack.js
@@ -0,0 +1,29 @@
+class MinStack {
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(x) {
+    let newMinMax = { min: x, max: x };
+    if (this.minStack.length > 0) {
+      let lastMinMax = this.minStack[this.minStack.length - 1];
+      newMinMax.min = Math.min(lastMinMax.min, x);
+      newMinMax.max = Math.max(lastMinMax.max, x);
+    }
+    this.minStack.push(newMinMax);
+    this.stack.push(x);
+  }
+
+  pop() {
+    this.minStack.pop();
+    return this.stack.pop();
+  }
+  //top = peek
+  top() {
+    return this.stack[this.stack.length - 1];
+  }
+  getMin() {
+    return this.minStack[this.minStack.length - 1].min;
+  }
+}
