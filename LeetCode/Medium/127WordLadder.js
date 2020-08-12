312f7d9afc1c24ebedaae409b473bcc2115613b5
Mike-Damato <damatomike@gmail.com>
2020-08-11 21:29:35 -0400

    Add - 127 Word Ladder

---
+++ LeetCode/Medium/127WordLadder.js
@@ -0,0 +1,31 @@
+var ladderLength = function (beginWord, endWord, wordList) {
+  let set = new Set(wordList);
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let step = 1;
+  let queue = [beginWord];
+  // let size;
+
+  while (queue.length) {
+    let nextQ = [];
+    for (let word of queue) {
+      if (word === endWord) {
+        return step;
+      }
+      for (let i = 0; i < word.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+          let wordArr = word.split('');
+          wordArr[i] = alphabet[j];
+          let newWord = wordArr.join('');
+
+          if (set.has(newWord)) {
+            nextQ.push(newWord);
+            set.delete(newWord);
+          }
+        }
+      }
+    }
+    queue = nextQ;
+    step++;
+  }
+  return 0;
+};
