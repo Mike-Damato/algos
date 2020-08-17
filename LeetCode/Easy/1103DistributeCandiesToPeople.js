b5d30b31e26f853fdcbc14f3a37ad9b67a7f8025
Mike-Damato <damatomike@gmail.com>
2020-08-17 09:59:27 -0400

    Add - 1103 Distribute Candies to People

---
+++ LeetCode/Easy/1103DistributeCandiesToPeople.js
@@ -0,0 +1,11 @@
+var distributeCandies = function (candies, num_people) {
+  let arr = new Array(num_people).fill(0);
+  let i = 0;
+  while (candies - i > 0) {
+    arr[i % num_people] += i + 1;
+    i++;
+    candies -= i;
+  }
+  arr[i % num_people] += candies;
+  return arr;
+};
