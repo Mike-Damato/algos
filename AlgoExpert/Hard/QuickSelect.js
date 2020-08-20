96ce2a0ae0b0fdb7f78fd2958a53621a4b17532e
Mike-Damato <damatomike@gmail.com>
2020-08-20 14:59:44 -0400

    Add - Quickselect

---
+++ AlgoExpert/Hard/QuickSelect.js
@@ -0,0 +1,34 @@
+function quickselect(array, k) {
+  // Write your code here.
+  let position = k - 1;
+  return helper(array, 0, array.length - 1, position);
+}
+
+const helper = (arr, start, end, position) => {
+  let pivot, left, right;
+  while (true) {
+    if (start > end) {
+      throw new Error('Error');
+    }
+    pivot = start;
+    left = start + 1;
+    right = end;
+    while (left <= right) {
+      if (arr[left] > arr[pivot] && arr[right] < arr[pivot]) {
+        swap(arr, left, right);
+      } else if (arr[right] >= arr[pivot]) {
+        right--;
+      } else if (arr[left] <= arr[pivot]) {
+        left++;
+      }
+    }
+    swap(arr, pivot, right);
+    if (right === position) {
+      return arr[right];
+    } else if (right < position) {
+      start = right + 1;
+    } else {
+      end = right - 1;
+    }
+  }
+};
