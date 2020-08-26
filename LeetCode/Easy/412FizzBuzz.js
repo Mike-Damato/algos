f10400a81f904b0ba6fd8f50719cfac10374b09c
Mike-Damato <damatomike@gmail.com>
2020-08-26 10:00:25 -0400

    Add - FizzBuzz Alternate Solution

--- LeetCode/Easy/412FizzBuzz.js
+++ LeetCode/Easy/412FizzBuzz.js
@@ -15,3 +15,23 @@ var fizzBuzz = function (n) {
   }
   return ans;
 };
+
+//Alternate using a hash
+const fizzBuzz = (n) => {
+  let ans = [];
+  let hash = { 3: 'Fizz', 5: 'Buzz' };
+
+  for (let i = 1; i <= n; i++) {
+    let innerResult = '';
+    for (let key in hash) {
+      if (i % key === 0) {
+        innerResult += hash[key];
+      }
+    }
+    if (innerResult === '') {
+      innerResult = `${i}`;
+    }
+    ans.push(innerResult);
+  }
+  return ans;
+};
