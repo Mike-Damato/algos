7d72f6da8257f47a6aa1a0bcfa0277dcbd8102dc
Mike-Damato <damatomike@gmail.com>
2020-08-26 12:07:56 -0400

    Add - ES6 Solution

--- LeetCode/Medium/1396UndergroundSystem.js
+++ LeetCode/Medium/1396UndergroundSystem.js
@@ -29,3 +29,30 @@ UndergroundSystem.prototype.getAverageTime = function (
 
   return sum / this.trips[route].length;
 };
+
+//Alternate using ES6
+class UndergroundSystem {
+  constructor() {
+    this.checkIns = {};
+    this.trips = {};
+  }
+  checkIn(id, stationName, t) {
+    this.checkIns[id] = { t, stationName };
+  }
+  checkOut(id, stationName, t) {
+    let route = this.checkIns[id].stationName + ' to ' + stationName;
+    if (!this.trips.hasOwnProperty(route)) {
+      this.trips[route] = [];
+    }
+    this.trips[route].push(t - this.checkIns[id].t);
+  }
+  getAverageTime(startStation, endStation) {
+    let route = startStation + ' to ' + endStation;
+    let sum = this.trips[route].reduce((a, b) => a + b, 0);
+
+    // for(let i = 0; i < this.trips[route].length; i++){
+    //     sum += this.trips[route][i]
+    // }
+    return sum / this.trips[route].length;
+  }
+}
