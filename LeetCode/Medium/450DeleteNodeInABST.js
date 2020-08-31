095d2caf5cd95326c1548ba5835b3dbc8e94aaf0
Mike-Damato <damatomike@gmail.com>
2020-08-31 09:51:55 -0400

    Add - 450 Delete Node in a BST

---
+++ LeetCode/Medium/450DeleteNodeInABST.js
@@ -0,0 +1,39 @@
+var deleteNode = function (root, key) {
+  if (!root) {
+    return null;
+  }
+
+  if (root.val === key) {
+    //If only 1 node
+    if (!root.left && !root.right) {
+      return null;
+    }
+    if (!root.right) {
+      return root.left;
+    }
+
+    const nextNode = helper(root.right);
+    root.val = nextNode.val;
+    root.right = deleteNode(root.right, nextNode.val);
+    return root;
+  }
+
+  if (root.val > key) {
+    root.left = deleteNode(root.left, key);
+  }
+  if (root.val < key) {
+    root.right = deleteNode(root.right, key);
+  }
+
+  return root;
+};
+
+const helper = (node) => {
+  if (!node) {
+    return null;
+  }
+  while (node.left) {
+    node = node.left;
+  }
+  return node;
+};
