7ede10150ce90b1bfa5b2d55e9bdd5d09202e85f
Mike-Damato <damatomike@gmail.com>
2020-09-01 14:09:21 -0400

    Add - 1297 Alternate Solution

--- LeetCode/Medium/1297MaxNumberOfOccurrencesOfASubstring.js
+++ LeetCode/Medium/1297MaxNumberOfOccurrencesOfASubstring.js
@@ -21,3 +21,30 @@ var maxFreq = function (s, maxLetters, minSize, maxSize) {
   }
   return max;
 };
+
+//Alternate
+const maxFreq = (s, maxLetters, minSize, maxSize) => {
+  let map = new Map();
+  let sub, set;
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    sub = s.substring(i, minSize + i);
+
+    if (sub.length >= minSize) {
+      set = new Set(sub.split(''));
+
+      if (set.size <= maxLetters) {
+        if (map.has(sub)) {
+          map.set(sub, map.get(sub) + 1);
+        } else {
+          map.set(sub, 1);
+        }
+      }
+    }
+  }
+
+  for (const [key, val] of map) {
+    max = Math.max(max, val);
+  }
+  return max;
+};
