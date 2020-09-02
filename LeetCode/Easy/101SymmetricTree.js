46738d846f37df95afe5742d85d0eb442e7a4915
Mike-Damato <damatomike@gmail.com>
2020-09-02 13:06:26 -0400

    Add - 101 Symmetric Tree Iterative & 220 Contains Duplicate

--- LeetCode/Easy/101SymmetricTree.js
+++ LeetCode/Easy/101SymmetricTree.js
@@ -12,3 +12,32 @@ var isSymmetric = function (root) {
   };
   return isMirror(root, root);
 };
+
+//Iterative
+const isSymmetric = (root) => {
+  if (!root) {
+    return true;
+  }
+
+  let queue = [root, root];
+  let size, current;
+  let node1, node2;
+  while (queue.length) {
+    node1 = queue.shift();
+    node2 = queue.shift();
+    if (!node1 && !node2) {
+      continue;
+    }
+    if (!node1 || !node2) {
+      return false;
+    }
+    if (node1.val !== node2.val) {
+      return false;
+    }
+    queue.push(node1.left);
+    queue.push(node2.right);
+    queue.push(node1.right);
+    queue.push(node2.left);
+  }
+  return true;
+};
--- LeetCode/Medium/220ContainsDuplicate.js
+++ LeetCode/Medium/220ContainsDuplicate.js
@@ -9,3 +9,31 @@ var containsNearbyAlmostDuplicate = function (nums, k, t) {
   }
   return false;
 };
+
+//Using Bucket Sort O(N) Time
+var containsNearbyAlmostDuplicate = function (nums, k, t) {
+  if (k < 1 || t < 0) {
+    return false;
+  }
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let bucket = Math.floor(nums[i] / (t + 1));
+    if (map.has(bucket)) {
+      return true;
+    }
+    if (map.has(bucket - 1) && Math.abs(nums[i] - map.get(bucket - 1)) <= t) {
+      return true;
+    }
+    if (map.has(bucket + 1) && Math.abs(map.get(bucket + 1) - nums[i]) <= t) {
+      return true;
+    }
+    if (map.size >= k) {
+      let outOfRange = Math.floor(nums[i - k] / (t + 1));
+      map.delete(outOfRange);
+    }
+
+    map.set(bucket, nums[i]);
+    //console.log(map)
+  }
+  return false;
+};
