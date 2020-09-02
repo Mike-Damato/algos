d164e13c06ff20b0999d976f5ad8f2113090aefd
Mike-Damato <damatomike@gmail.com>
2020-09-02 14:53:49 -0400

    114 Flatten Binary Tree To Linked List Solutions

--- LeetCode/Medium/114FlattenBinaryTreeToLinkedList.js
+++ LeetCode/Medium/114FlattenBinaryTreeToLinkedList.js
@@ -15,3 +15,39 @@ var flatten = function (root) {
     current.left = null;
   }
 };
+
+//Recursive
+const flatten = (root) => {
+  let prev = null;
+  const flattenHelper = (node) => {
+    if (!node) {
+      return null;
+    }
+    flattenHelper(node.right);
+    flattenHelper(node.left);
+
+    node.left = null;
+    node.right = prev;
+    prev = node;
+  };
+  flattenHelper(root);
+};
+
+//Constant Space
+var flatten = function (root) {
+  if (root === null) {
+    return null;
+  }
+  let stack = [root];
+  while (stack.length) {
+    let current = stack.pop();
+    if (current.right) {
+      stack.push(current.right);
+    }
+    if (current.left) {
+      stack.push(current.left);
+    }
+    current.right = stack[stack.length - 1];
+    current.left = null;
+  }
+};
