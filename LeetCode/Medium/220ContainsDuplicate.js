0e02e481467ebc75a2a3828cdacb7170e08acd2d
Mike-Damato <damatomike@gmail.com>
2020-09-02 10:11:47 -0400

    Add - 220 Contains Duplicate

---
+++ LeetCode/Medium/220ContainsDuplicate.js
@@ -0,0 +1,11 @@
+//Naive O(N^2) Time Approach
+var containsNearbyAlmostDuplicate = function (nums, k, t) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) <= t && Math.abs(i - j) <= k) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
