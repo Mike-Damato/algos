a77a683598342537913103d104a9c46564c0aef7
Mike-Damato <damatomike@gmail.com>
2020-09-07 11:00:20 -0400

    Add - 64 Minimum Path Sum

--- LeetCode/Medium/64MinimumPathSum.js
+++ LeetCode/Medium/64MinimumPathSum.js
@@ -31,3 +31,21 @@ var minPathSum = function (grid) {
   return dpGrid[grid.length - 1][grid[0].length - 1];
   // console.log(dpGrid)
 };
+
+//Alternate
+
+var minPathSum = function (grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (i !== 0 && j !== 0) {
+        grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+      } else if (i !== 0) {
+        grid[i][j] += grid[i - 1][j];
+      } else if (j !== 0) {
+        grid[i][j] += grid[i][j - 1];
+      }
+    }
+  }
+
+  return grid[grid.length - 1].pop();
+};
