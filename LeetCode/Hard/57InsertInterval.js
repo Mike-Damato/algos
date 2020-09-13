542a7cda397696a3a42af3e9ea2fcd178220524d
Mike-Damato <damatomike@gmail.com>
2020-09-13 11:46:57 -0400

    Add - 57 Insert Interval

---
+++ LeetCode/Hard/57InsertInterval.js
@@ -0,0 +1,23 @@
+var insert = function (intervals, newInterval) {
+  let n = intervals.length;
+  let idx = 0;
+  let ans = [];
+
+  while (idx < n && intervals[idx][1] < newInterval[0]) {
+    ans.push(intervals[idx]);
+    idx++;
+  }
+
+  while (idx < n && intervals[idx][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[idx][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[idx][1]);
+    idx++;
+  }
+  ans.push(newInterval);
+
+  while (idx < n) {
+    ans.push(intervals[idx]);
+    idx++;
+  }
+  return ans;
+};
