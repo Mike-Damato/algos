77998693e534925d595dc81586e17471750bd357
Mike-Damato <damatomike@gmail.com>
2020-10-03 12:47:28 -0400

    Add - 872 Leaf Similar Trees

---
+++ LeetCode/Easy/872LeafSimilarTrees.js
@@ -0,0 +1,23 @@
+const leafSimilar = (root1, root2) => {
+  const dfs = (node, arr) => {
+    if (!node) return;
+
+    if (!node.left && !node.right) {
+      arr.push(node.val);
+    }
+    dfs(node.left, arr);
+    dfs(node.right, arr);
+  };
+
+  let tree1 = [];
+  let tree2 = [];
+  dfs(root1, tree1);
+  dfs(root2, tree2);
+
+  if (tree1.length !== tree2.length) return false;
+
+  for (let i = 0; i < tree1.length; i++) {
+    if (tree1[i] !== tree2[i]) return false;
+  }
+  return true;
+};
