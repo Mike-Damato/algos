c0fd3bfe9d91cbcc1a9bcfe7584c2e96c3cb06ff
Mike-Damato <damatomike@gmail.com>
2020-10-12 11:23:22 -0400

    Add - 859 Buddy Strings

---
+++ LeetCode/Easy/859BuddyStrings.js
@@ -0,0 +1,19 @@
+var buddyStrings = function (A, B) {
+  if (A.length !== B.length) return false;
+  if (A === B) {
+    let set = new Set([...A]);
+    console.log(set);
+    return set.size < A.length;
+  }
+
+  let arr = [];
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] !== B[i]) {
+      arr.push(i);
+    }
+  }
+
+  if (arr.length !== 2) return false;
+
+  return A[arr[0]] === B[arr[1]] && A[arr[1]] === B[arr[0]];
+};
