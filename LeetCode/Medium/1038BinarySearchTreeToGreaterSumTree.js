593d415e32171a19f093736ce60326f5f38bbc39
Mike-Damato <damatomike@gmail.com>
2020-10-15 14:48:43 -0400

    Add - 1038 Binary Search Tree To Greater Sum Tree

---
+++ LeetCode/Medium/1038BinarySearchTreeToGreaterSumTree.js
@@ -0,0 +1,12 @@
+var bstToGst = function (root) {
+  let sum = 0;
+  const dfs = (node) => {
+    if (!node) return;
+    dfs(node.right);
+    sum += node.val;
+    node.val = sum;
+    dfs(node.left);
+  };
+  dfs(root);
+  return root;
+};
