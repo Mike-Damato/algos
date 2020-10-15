a6d6af5328f622a4e20f8b9007da59d5dbe7992f
Mike-Damato <damatomike@gmail.com>
2020-10-15 14:18:32 -0400

    Fix - 1570 Syntax

--- LeetCode/Medium/1570DotProductOfTwoSparseVectors.js
+++ LeetCode/Medium/1570DotProductOfTwoSparseVectors.js
@@ -1,12 +1,12 @@
- */
-class SparseVector{
-    constructor(nums){
-        this.nums = nums
+class SparseVector {
+  constructor(nums) {
+    this.nums = nums;
+  }
+  dotProduct(vec) {
+    let ans = 0;
+    for (let i = 0; i < this.nums.length; i++) {
+      ans += this.nums[i] * vec[i];
     }
-    dotProduct(vec){
-        let ans = 0
-        for(let i = 0; i < this.nums.length; i++){
-            ans += this.nums[i] * vec[i];
-    }
-        return ans;
+    return ans;
+  }
 }
