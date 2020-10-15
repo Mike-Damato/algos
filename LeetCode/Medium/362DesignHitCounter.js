cc5fdaa02a76ea1a5f7bcf29cb1026adee06af7e
Mike-Damato <damatomike@gmail.com>
2020-10-14 20:05:33 -0400

    Add - 362 Alt Solution using Binary Search

--- LeetCode/Medium/362DesignHitCounter.js
+++ LeetCode/Medium/362DesignHitCounter.js
@@ -9,8 +9,45 @@ class HitCounter {
     let idx = 0;
     while (this.hits[idx] < timestamp - 299) {
       this.hits.shift();
-      console.log(this.hits, timestamp - 299);
     }
     return this.hits.length;
+  }
+}
+
+//Binary Search
+class HitCounter {
+  constructor() {
+    this.hits = [];
+  }
+  hit(timestamp) {
+    this.hits.push(timestamp);
+  }
+  getHits(timestamp) {
+    let [left, right] = this.binarySearch(
+      0,
+      this.hits.length - 1,
+      this.hits,
+      timestamp - 300
+    );
+    let target = timestamp - 300;
+    if (this.hits[left] > target) {
+      return this.hits.length - left;
+    } else if (this.hits[right] > target) {
+      return this.hits.length - right;
+    }
+
+    return 0;
+  }
+  binarySearch(left, right, arr, target) {
+    while (left + 1 < right) {
+      let mid = left + Math.floor((right - left) / 2);
+      let val = arr[mid];
+      if (val > target) {
+        right = mid;
+      } else {
+        left = mid;
+      }
+    }
+    return [left, right];
   }
 }
