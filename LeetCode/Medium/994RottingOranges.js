7d2ce1cc70c6b7a7dfb5d517666fe8da4e51b221
Mike-Damato <damatomike@gmail.com>
2020-10-19 13:30:04 -0400

    Add - 994 Rotting Oranges Alternate Solution

--- LeetCode/Medium/994RottingOranges.js
+++ LeetCode/Medium/994RottingOranges.js
@@ -55,3 +55,67 @@ const search = (grid, i, j, queue) => {
   }
   return bad;
 };
+
+//Alternate
+var orangesRotting = function (grid) {
+  /*
+    first locate the rotten oranges
+    it will be a queue of rotten oranges
+    each element would be an array of cell and num of minutes that passes   [cell(row,col), min]
+    each iteration is at min
+    1. locate rotten oranges and add them to queue in format of [row, col, step]
+    2. have a count of fresh oranges
+    3. have a variable for maxTime
+    4. start bfs via while loop if queue.length > 0
+    5. create helper inBounds func
+    6. traverse thru 4 neighbors of current cell and check if newCell is in bounds
+    7. if cell inBounds, add to queue and increment step
+    8. check fresh count and maxTime
+    */
+  // add rotten oranges into queue
+  let freshCount = 0;
+  let queue = [];
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 2) {
+        queue.push([i, j, 0]);
+      } else if (grid[i][j] === 1) {
+        freshCount += 1;
+      }
+    }
+  }
+  function inBounds(row, col) {
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  // bfs
+  let maxTime = 0;
+  let dirs = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+  while (queue.length) {
+    // check 4 sides for fresh oranges, once its added its rotten
+    const currentOrange = queue.shift();
+    const [row, col, step] = currentOrange;
+    maxTime = step;
+    for (let dir of dirs) {
+      const [x, y] = dir;
+      const newRow = x + row;
+      const newCol = y + col;
+      //Check if we are in bounds and if any neighboring oranges are fresh, if they are they now become rotten so decrement freshcount
+      if (inBounds(newRow, newCol) && grid[newRow][newCol] === 1) {
+        queue.push([newRow, newCol, step + 1]);
+        grid[newRow][newCol] = 2;
+        freshCount -= 1;
+      }
+    }
+    // console.log(queue)
+  }
+  return freshCount === 0 ? maxTime : -1;
+};
