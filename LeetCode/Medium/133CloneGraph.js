69fb4075cd5bfd18b093118968030d8f160102a2
Mike-Damato <damatomike@gmail.com>
2020-10-20 11:32:51 -0400

    Add - 133 BFS Solution

--- LeetCode/Medium/133CloneGraph.js
+++ LeetCode/Medium/133CloneGraph.js
@@ -15,8 +15,31 @@ var cloneGraph = function (node) {
     for (const neighbor of node.neighbors) {
       copy.neighbors.push(dfs(neighbor, map));
     }
-
     return copy;
   };
   return dfs(node, map);
+};
+
+//BFS Solution
+const cloneGraph = (node) => {
+  if (!node) return null;
+  let map = new Map();
+
+  const bfs = (node, map) => {
+    map.set(node, new Node(node.val));
+    const queue = [node];
+
+    while (queue.length > 0) {
+      let current = queue.shift();
+      for (const neighbor of current.neighbors) {
+        if (!map.has(neighbor)) {
+          map.set(neighbor, new Node(neighbor.val));
+          queue.push(neighbor);
+        }
+        map.get(current).neighbors.push(map.get(neighbor));
+      }
+    }
+    return map.get(node);
+  };
+  return bfs(node, map);
 };
