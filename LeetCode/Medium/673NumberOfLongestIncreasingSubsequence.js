d7b73865000ca90f2df1e841fc7ad3bd3e6f3d50
Mike-Damato <damatomike@gmail.com>
2020-10-30 15:48:11 -0400

    Add - 673 Number Of Longest Increasing Subsequences

---
+++ LeetCode/Medium/673NumberOfLongestIncreasingSubsequence.js
@@ -0,0 +1,27 @@
+var findNumberOfLIS = function (nums) {
+  const n = nums.length;
+  const dp = new Array(n).fill(1);
+  const counts = new Array(n).fill(1);
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        if (dp[j] + 1 == dp[i]) {
+          counts[i] += counts[j];
+        } else if (dp[j] + 1 > dp[i]) {
+          dp[i] = dp[j] + 1;
+          counts[i] = counts[j];
+        }
+      }
+    }
+  }
+
+  const max = Math.max(...dp);
+  let ans = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (dp[i] == max) ans += counts[i];
+  }
+
+  return ans;
+};
