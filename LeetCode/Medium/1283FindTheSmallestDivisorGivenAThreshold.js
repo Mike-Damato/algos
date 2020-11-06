754300a1efc9f471671df329cd16ed49108b7bc6
Mike-Damato <damatomike@gmail.com>
2020-11-06 12:54:33 -0500

    Add - 1283 Find The Smallest Divisor Given A Threshold

---
+++ LeetCode/Medium/1283FindTheSmallestDivisorGivenAThreshold.js
@@ -0,0 +1,22 @@
+var smallestDivisor = function (nums, threshold) {
+  let min = 1;
+  let max = nums.reduce((a, b) => a + b, 0);
+  return binarySearch(min, max, nums, threshold);
+};
+
+const binarySearch = (left, right, arr, target) => {
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    let currentDiv = arr.reduce((a, b) => {
+      return a + Math.ceil(b / mid);
+    }, 0);
+
+    if (currentDiv > target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+};
