5bed0b316a35a0c854968e7a69ec71e325b50288
Mike-Damato <damatomike@gmail.com>
2020-11-11 13:24:44 -0500

    Add- 821 Shortest Distance To A Character

---
+++ LeetCode/Easy/821ShortestDistanceToACharacter.js
@@ -0,0 +1,23 @@
+var shortestToChar = function (S, C) {
+  const dp = new Array(S.length).fill(Infinity);
+
+  dp[0] = S[0] === C ? 0 : Infinity;
+
+  for (let i = 1; i < S.length; i++) {
+    if (S[i] === C) {
+      dp[i] = 0;
+    } else {
+      dp[i] = dp[i - 1] === Infinity ? Infinity : dp[i - 1] + 1;
+    }
+  }
+
+  let dist = Infinity;
+  for (let i = S.length - 1; i >= 0; i--) {
+    if (S[i] === C) {
+      dist = 0;
+    }
+    dp[i] = Math.min(dist, dp[i]);
+    dist += 1;
+  }
+  return dp;
+};
