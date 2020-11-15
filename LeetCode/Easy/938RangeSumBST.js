c20cd0620ca934a1a3df6064fba642e918797e21
Mike-Damato <damatomike@gmail.com>
2020-11-15 10:13:07 -0500

    Add - 938 Alt Solution using BFS

--- LeetCode/Easy/938RangeSumBST.js
+++ LeetCode/Easy/938RangeSumBST.js
@@ -17,3 +17,27 @@ var rangeSumBST = function (root, low, high) {
   dfs(root);
   return sum;
 };
+
+//BFS
+const rangeSumBST = (root, low, high) => {
+  if (!root) return null;
+  let queue = [root];
+  let size, current;
+  let sum = 0;
+  while (queue.length) {
+    size = queue.length;
+    for (let i = 0; i < size; i++) {
+      current = queue.shift();
+      if (current.val >= low && current.val <= high) {
+        sum += current.val;
+      }
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+  }
+  return sum;
+};
