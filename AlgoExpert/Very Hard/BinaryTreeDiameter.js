5adce28f8cf38a205536bab1067ca783d70621ee
Mike-Damato <damatomike@gmail.com>
2020-11-17 12:44:06 -0500

    Add - Binary Tree Diameter Alternate Solution

--- AlgoExpert/Very Hard/BinaryTreeDiameter.js
+++ AlgoExpert/Very Hard/BinaryTreeDiameter.js
@@ -29,3 +29,26 @@ const dfs = (node) => {
 
   return new TreeInfo(currentDiameter, currentHeight);
 };
+
+//Alternate
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function binaryTreeDiameter(tree) {
+  let ans = 1;
+  const dfs = (node) => {
+    if (!node) return 0;
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+    ans = Math.max(ans, left + right + 1);
+    return Math.max(left, right) + 1;
+  };
+  dfs(tree);
+  return ans - 1;
+}
