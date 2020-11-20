d219a58815b279709e808112182d8ba586bf46cd
Mike-Damato <damatomike@gmail.com>
2020-11-20 16:00:14 -0500

    Add - Waterfall Streams

---
+++ AlgoExpert/Very Hard/WaterfallStreams.js
@@ -0,0 +1,51 @@
+//O(w^2 * h) Time & O(w) space
+function waterfallStreams(array, source) {
+  let rowAbove = [...array[0]];
+  rowAbove[source] = -1;
+
+  for (let row = 1; row < array.length; row++) {
+    const currentRow = [...array[row]];
+    for (let idx = 0; idx < rowAbove.length; idx++) {
+      const valAbove = rowAbove[idx];
+
+      const hasWaterAbove = valAbove < 0;
+      const hasBlock = currentRow[idx] === 1;
+
+      if (!hasWaterAbove) {
+        continue;
+      }
+      if (!hasBlock) {
+        currentRow[idx] += valAbove;
+        continue;
+      }
+
+      const splitWater = valAbove / 2;
+
+      let rightIdx = idx;
+      while (rightIdx + 1 < rowAbove.length) {
+        rightIdx++;
+        if (rowAbove[rightIdx] === 1) {
+          break;
+        }
+        if (currentRow[rightIdx] !== 1) {
+          currentRow[rightIdx] += splitWater;
+          break;
+        }
+      }
+
+      let leftIdx = idx;
+      while (leftIdx - 1 >= 0) {
+        leftIdx--;
+        if (rowAbove[leftIdx] === 1) {
+          break;
+        }
+        if (currentRow[leftIdx] !== 1) {
+          currentRow[leftIdx] += splitWater;
+          break;
+        }
+      }
+    }
+    rowAbove = currentRow;
+  }
+  return rowAbove.map((num) => (num < 0 ? num * -100 : num));
+}
