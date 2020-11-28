7f6196961cd6aac60fcfc4031dc7edc4602206b3
Mike-Damato <damatomike@gmail.com>
2020-11-28 11:39:55 -0500

    Add - 239 Alternate Solution

--- LeetCode/Hard/239SlidingWindowMaximum.js
+++ LeetCode/Hard/239SlidingWindowMaximum.js
@@ -1,5 +1,18 @@
 var maxSlidingWindow = function (nums, k) {
   const ans = [];
+  for (let i = 0; i < nums.length - k + 1; i++) {
+    let max = -Infinity;
+    for (let j = i; j < i + k; j++) {
+      max = Math.max(max, nums[j]);
+    }
+    ans.push(max);
+  }
+  return ans;
+};
+
+//Optimized - Note using an array to represent a queue...
+var maxSlidingWindow = function (nums, k) {
+  const ans = [];
   let queue = nums.slice(0, k);
   let right = k - 1;
   let max;
