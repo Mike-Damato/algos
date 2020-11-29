c99fedb76ba2f7a748aaf38ca2cea849def6725e
Mike-Damato <damatomike@gmail.com>
2020-11-29 08:40:11 -0500

    Add - 1306 Alternate Solution

--- LeetCode/Medium/1306JumpGame3.js
+++ LeetCode/Medium/1306JumpGame3.js
@@ -1,3 +1,4 @@
+//BFS
 var canReach = function (arr, start) {
   const queue = [start];
   let current;
@@ -17,6 +18,20 @@ var canReach = function (arr, start) {
     }
     //Mark visited
     arr[current] = -arr[current];
+  }
+  return false;
+};
+
+//Alternate solution using DFS
+var canReach = function (arr, start) {
+  if (start >= 0 && start < arr.length && arr[start] >= 0) {
+    if (arr[start] === 0) return true;
+
+    arr[start] = -arr[start];
+
+    return (
+      canReach(arr, start + arr[start]) || canReach(arr, start - arr[start])
+    );
   }
   return false;
 };
