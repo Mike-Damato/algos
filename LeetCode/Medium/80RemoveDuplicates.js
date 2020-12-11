7e7a549daf52989fcad7435b76cacccaf0a97ddc
Mike-Damato <damatomike@gmail.com>
2020-12-11 12:22:27 -0500

    Add - 80 Remove Duplicates

---
+++ LeetCode/Medium/80RemoveDuplicates.js
@@ -0,0 +1,17 @@
+var removeDuplicates = function (nums) {
+  let j = 1;
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
+
+    if (count <= 2) {
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+  return j;
+};
