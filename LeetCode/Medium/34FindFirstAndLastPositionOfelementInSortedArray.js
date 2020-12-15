fb5a99c487d9e4d36e47102b8943b5565bd777ec
Mike-Damato <damatomike@gmail.com>
2020-12-15 13:47:53 -0500

    Add - 34 Alternate Solution

--- LeetCode/Medium/34FindFirstAndLastPositionOfelementInSortedArray.js
+++ LeetCode/Medium/34FindFirstAndLastPositionOfelementInSortedArray.js
@@ -33,3 +33,42 @@ var searchRange = function (nums, target) {
   range[1] = left;
   return range;
 };
+
+//Atlernate
+var searchRange = function (nums, target) {
+  // Write your code here.
+  let range = [-1, -1];
+  let left = 0;
+  let right = nums.length - 1;
+  binarySearch(nums, left, right, target, range, true);
+  binarySearch(nums, left, right, target, range, false);
+  return range;
+};
+
+const binarySearch = (arr, left, right, target, range, goLeft) => {
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (arr[mid] === target) {
+      if (goLeft) {
+        if (mid === 0 || arr[mid - 1] !== target) {
+          range[0] = mid;
+          return;
+        } else {
+          right = mid - 1;
+        }
+      } else {
+        if (mid === arr.length - 1 || arr[mid + 1] !== target) {
+          range[1] = mid;
+          return;
+        } else {
+          left = mid + 1;
+        }
+      }
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+};
