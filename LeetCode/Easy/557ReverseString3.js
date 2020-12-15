8132091c3ae1fd4412c2f941d77c60ddd0a88f5e
Mike-Damato <damatomike@gmail.com>
2020-12-14 20:29:34 -0500

    Add - 557 Reverse String 3 Alternate Solution

--- LeetCode/Easy/557ReverseString3.js
+++ LeetCode/Easy/557ReverseString3.js
@@ -7,3 +7,23 @@ var reverseWords = function (s) {
   }
   return answer.trim();
 };
+
+//Alternate
+var reverseWords = function (s) {
+  let reversed = '';
+  for (let i = s.length - 1; i >= 0; i--) {
+    reversed += s[i];
+  }
+  return reverseIt(reversed.split(' ')).join(' ');
+};
+
+const reverseIt = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+  return arr;
+};
