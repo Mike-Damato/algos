1d97d4d0b93cffe12ec3c1220e4c6851a452ff31
Mike-Damato <damatomike@gmail.com>
2020-12-21 12:39:49 -0500

    Add - 1506 Alternate Solution

--- LeetCode/Medium/1506FindRootOfN-AryTree.js
+++ LeetCode/Medium/1506FindRootOfN-AryTree.js
@@ -11,3 +11,18 @@ var findRoot = function (tree) {
     if (!set.has(node.val)) return node;
   }
 };
+
+//Alternate with O(1) space
+var findRoot = function (tree) {
+  let sum = 0;
+  for (const node of tree) {
+    sum += node.val;
+    for (const child of node.children) {
+      sum -= child.val;
+    }
+  }
+
+  for (const node of tree) {
+    if (node.val === sum) return node;
+  }
+};
