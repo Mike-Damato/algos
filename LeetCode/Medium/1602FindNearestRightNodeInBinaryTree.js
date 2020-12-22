beb44839a53944bd1b8205d1e71b999bb957a86f
Mike-Damato <damatomike@gmail.com>
2020-12-22 12:30:22 -0500

    Add - 1602 Find Nearest Right Node In Binary Tree Alternate Solution

--- LeetCode/Medium/1602FindNearestRightNodeInBinaryTree.js
+++ LeetCode/Medium/1602FindNearestRightNodeInBinaryTree.js
@@ -23,3 +23,25 @@ var findNearestRightNode = function (root, u) {
   }
   return null;
 };
+
+//Alternate
+var findNearestRightNode = function (root, u) {
+  if (!root) return null;
+  const queue = [root];
+  let current, size;
+  while (queue.length) {
+    size = queue.length;
+    for (let i = 0; i < size; i++) {
+      current = queue.shift();
+      if (current === u) {
+        return i < size - 1 ? queue[0] : null;
+      }
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+  }
+};
