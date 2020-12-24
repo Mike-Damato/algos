2faa395f92518809df762a200ed7670bf2cf9329
Mike-Damato <damatomike@gmail.com>
2020-12-24 12:43:41 -0500

    Add - 24 Swap Nodes In Pairs Iterative Approach

--- LeetCode/Medium/24SwapNodesInPairs.js
+++ LeetCode/Medium/24SwapNodesInPairs.js
@@ -11,3 +11,24 @@ var swapPairs = function (head) {
 
   return node2;
 };
+
+//Iterative Approach
+const swapPairs = (head) => {
+  if (!head || !head.next) return head;
+
+  let senty = new ListNode(0);
+  let dummy = senty;
+  senty.next = head;
+
+  while (head && head.next) {
+    let firstNode = head;
+    let secondNode = head.next;
+
+    dummy.next = secondNode;
+    firstNode.next = secondNode.next;
+    secondNode.next = firstNode;
+    dummy = firstNode;
+    head = firstNode.next;
+  }
+  return senty.next;
+};
