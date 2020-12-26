e9dc18f04e2cce063cf1a288c4750d49099c9313
Mike-Damato <damatomike@gmail.com>
2020-12-26 14:21:08 -0500

    Add - 91 Decode Ways Review

--- LeetCode/Medium/91DecodeWays.js
+++ LeetCode/Medium/91DecodeWays.js
@@ -33,3 +33,39 @@ function numDecodings(s) {
 
   return helper(0);
 }
+
+//Review Decode Ways
+const numDecodings = (s) => {
+  const map = new Map();
+
+  const helper = (idx) => {
+    if (idx === s.length) return 1;
+
+    if (idx > s.length) return 0;
+
+    //Check if we've already seen this
+    if (map.has(idx)) {
+      return map.get(idx);
+    }
+
+    let totalCombinations = 0;
+    //For 1 digit nums
+    const oneDigit = s.slice(idx, idx + 1);
+    //For 2 digit nums
+    const twoDigit = s.slice(idx, idx + 2);
+
+    if (oneDigit !== '0') {
+      totalCombinations += helper(idx + 1);
+    }
+
+    if (twoDigit[0] !== '0' && Number(twoDigit) <= 26) {
+      totalCombinations += helper(idx + 2);
+    }
+
+    map.set(idx, totalCombinations);
+
+    return totalCombinations;
+  };
+
+  return helper(0);
+};
