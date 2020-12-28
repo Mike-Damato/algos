20aa2be3ef7a77da478bffb154a860fb877f4eb6
Mike-Damato <damatomike@gmail.com>
2020-12-28 15:07:49 -0500

    Add - 754 Reach A Number

---
+++ LeetCode/Medium/754ReachANumber.js
@@ -0,0 +1,11 @@
+var reachNumber = function (target) {
+  target = Math.abs(target);
+  let currentStep = 1;
+
+  while (target > 0) {
+    currentStep++;
+    target -= currentStep;
+  }
+
+  return target % 2 === 0 ? currentStep : currentStep + 1 + (currentStep % 2);
+};
