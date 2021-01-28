721ceca320e7944106179cd37147c945542aec51
Mike-Damato <damatomike@gmail.com>
2021-01-28 12:51:42 -0500

    Add - 1663 Smallest String With A Given Numeric Value additional solution

--- LeetCode/Medium/1663SmallestStringWithAGivenNumericValue.js
+++ LeetCode/Medium/1663SmallestStringWithAGivenNumericValue.js
@@ -9,5 +9,51 @@ var getSmallestString = function (n, k) {
     k -= Math.min(25, k);
     n -= 1;
   }
+
   return 'a'.repeat(n) + str;
+};
+
+const getSmallestString = (n, k) => {
+  const alphabet = [
+    '',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  let sum = 0;
+  let ans = '';
+
+  for (let i = n - 1; i >= 0; i--) {
+    let letterIdx = k - i - sum;
+    if (letterIdx > 26) {
+      letterIdx = 26;
+    }
+    ans = alphabet[letterIdx] + ans;
+    sum += letterIdx;
+  }
+  return ans;
 };
