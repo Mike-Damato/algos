dee4a10f6ab6d8bde3a7f3a7a40eff80ac890db2
Mike-Damato <damatomike@gmail.com>
2021-01-31 10:51:32 -0500

    Add - 31 Next Permutation

---
+++ LeetCode/Medium/31NextPermutation.js
@@ -0,0 +1,31 @@
+var nextPermutation = function (nums) {
+  let i = nums.length - 2;
+
+  //loop from back to front and find the first num that is decreasing
+  while (i >= 0 && nums[i + 1] <= nums[i]) {
+    i--;
+  }
+  let j = nums.length - 1;
+  //next perm swap nums[i] & nums[j] when j > nums[i]
+  if (i >= 0) {
+    while (j >= 0 && nums[j] <= nums[i]) {
+      j--;
+    }
+    swap(nums, i, j);
+  }
+
+  //Swap everyting back to go back to ascending order
+  let left = i + 1;
+  let right = nums.length - 1;
+  while (left < right) {
+    swap(nums, left, right);
+    left++;
+    right--;
+  }
+  return nums;
+};
+
+const swap = (arr, i, j) => {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+  return arr;
+};
