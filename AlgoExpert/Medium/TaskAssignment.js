52c05413d97482ea8c58a9e4e3dd235390f27e29
Mike-Damato <damatomike@gmail.com>
2021-02-07 11:34:07 -0500

    Fix - Task Assignment

--- AlgoExpert/Medium/TaskAssignment.js
+++ AlgoExpert/Medium/TaskAssignment.js
@@ -1,13 +1,31 @@
 function taskAssignment(k, tasks) {
+  // Write your code here.
   const ans = [];
-  tasks.sort((a, b) => a - b);
-  let left = 0;
-  let right = tasks.length - 1;
-  while (k > 0) {
-    ans.push([left, right]);
-    left++;
-    right--;
-    k--;
+  const map = buildMap(tasks);
+  const sorted = [...tasks].sort((a, b) => a - b);
+  for (let i = 0; i < k; i++) {
+    const task1Duration = sorted[i];
+    const task1Idxs = map.get(sorted[i]);
+    const task1Idx = task1Idxs.pop();
+
+    const task2Sorted = tasks.length - 1 - i;
+    const task2Duration = sorted[task2Sorted];
+    const task2Idxs = map.get(task2Duration);
+    const task2Idx = task2Idxs.pop();
+
+    ans.push([task1Idx, task2Idx]);
   }
   return ans;
 }
+
+const buildMap = (tasks) => {
+  const map = new Map();
+  for (let i = 0; i < tasks.length; i++) {
+    if (!map.has(tasks[i])) {
+      map.set(tasks[i], [i]);
+    } else {
+      map.get(tasks[i]).push(i);
+    }
+  }
+  return map;
+};
