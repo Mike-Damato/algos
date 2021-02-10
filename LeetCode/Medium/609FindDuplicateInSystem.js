6e23114cc3629b8191f1a881ea441666a9cf48b6
Mike-Damato <damatomike@gmail.com>
2021-02-10 13:19:55 -0500

    609 Find Duplicate In System

---
+++ LeetCode/Medium/609FindDuplicateInSystem.js
@@ -0,0 +1,15 @@
+var findDuplicate = function (paths) {
+  const map = new Map();
+  for (const path of paths) {
+    let files = path.split(' ');
+    for (let i = 1; i < files.length; i++) {
+      let open = files[i].indexOf('(');
+      let content = files[i].slice(open + 1, files[i].length - 1);
+      if (!map.has(content)) {
+        map.set(content, []);
+      }
+      map.get(content).push(`${files[0]}/${files[i].slice(0, open)}`);
+    }
+  }
+  return [...map.values()].filter((item) => item.length > 1);
+};
