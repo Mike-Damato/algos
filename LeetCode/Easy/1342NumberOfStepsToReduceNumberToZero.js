85f53a9acb4a075378258a4c20fd25fb8a19ed99
Mike-Damato <damatomike@gmail.com>
2021-02-12 12:46:45 -0500

    Add - 1342 Number Of Steps To Reduce Number To Zero Alternate Solution

--- LeetCode/Easy/1342NumberOfStepsToReduceNumberToZero.js
+++ LeetCode/Easy/1342NumberOfStepsToReduceNumberToZero.js
@@ -14,3 +14,14 @@ var numberOfSteps = function (num) {
 
   return getSteps(num, 0);
 };
+
+//All within 1 function
+const numberOfSteps = (num, steps = 0) => {
+  if (num === 0) return steps;
+
+  if (num % 2 === 0) {
+    return numberOfSteps(num / 2, steps + 1);
+  } else {
+    return numberOfSteps(num - 1, steps + 1);
+  }
+};
