9ae815af2fe4fff9aabe4a1c1399ac4151c0a5a2
Mike-Damato <damatomike@gmail.com>
2021-02-14 11:20:54 -0500

    Add - 785 Is Graph Bipartite BFS Solution

--- LeetCode/Medium/785IsGraphBipartite.js
+++ LeetCode/Medium/785IsGraphBipartite.js
@@ -21,3 +21,36 @@ const dfs = (graph, map, i, color) => {
 
   return true;
 };
+
+//BFS Solution
+const isBipartite = (graph) => {
+  const map = new Map();
+  for (let i = 0; i < graph.length; i++) {
+    if (!bfs(graph, map, i)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const bfs = (graph, map, i) => {
+  if (!map.has(i)) {
+    const queue = [i];
+    map.set(i, 0);
+
+    while (queue.length) {
+      i = queue.shift();
+      for (const node of graph[i]) {
+        if (map.get(node) === map.get(i)) {
+          return false;
+        }
+
+        if (!map.has(node)) {
+          queue.push(node);
+          map.set(node, 1 - map.get(i));
+        }
+      }
+    }
+  }
+  return true;
+};
