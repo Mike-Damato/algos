19b82b7e7531191ece9a64194762beae651579cb
Mike-Damato <damatomike@gmail.com>
2021-02-21 10:08:23 -0500

    Add - 991 Broken Calculator Recursive Solution

--- LeetCode/Medium/991BrokenCalculator.js
+++ LeetCode/Medium/991BrokenCalculator.js
@@ -10,3 +10,15 @@ var brokenCalc = function (X, Y) {
   }
   return steps + X - Y;
 };
+
+//Recursive
+const brokenCalc = (X, Y) => {
+  if (X >= Y) {
+    return X - Y;
+  }
+  const mid = Math.ceil(Y / 2);
+  if (X >= mid) {
+    return X - mid + (2 * mid - Y) + 1;
+  }
+  return brokenCalc(X, mid) + (2 * mid - Y) + 1;
+};
