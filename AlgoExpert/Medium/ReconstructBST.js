e43dbb2b0999f08c2fa04bf055bb37c625f4dee6
Mike-Damato <damatomike@gmail.com>
2021-02-24 12:50:30 -0500

    Add - Reconstruct BST Optimized Solution

--- AlgoExpert/Medium/ReconstructBST.js
+++ AlgoExpert/Medium/ReconstructBST.js
@@ -5,7 +5,7 @@ class BST {
     this.right = right;
   }
 }
-//O(N^2) Time | O(N) Space
+//O(N^2) Time | O(H) Space
 function reconstructBst(preorder) {
   if (!preorder || !preorder.length) return null;
   const value = preorder[0];
@@ -21,3 +21,40 @@ function reconstructBst(preorder) {
   const rightSubtree = reconstructBst(preorder.slice(rightSubtreeRootIdx));
   return new BST(value, leftSubtree, rightSubtree);
 }
+
+//Optimized solution
+class BST {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class TreeInfo {
+  constructor(root) {
+    this.root = root;
+  }
+}
+//O(N) Time | O(N) Space
+function reconstructBst(preorder) {
+  if (!preorder || !preorder.length) return null;
+  const newTree = new TreeInfo(0);
+  return reconstructBSTFromArray(-Infinity, Infinity, preorder, newTree);
+}
+
+const reconstructBSTFromArray = (left, right, preorder, newTree) => {
+  if (newTree.root === preorder.length) return null;
+
+  const rootVal = preorder[newTree.root];
+  if (rootVal <= left || rootVal >= right) return null;
+  newTree.root++;
+  const leftSubTree = reconstructBSTFromArray(left, rootVal, preorder, newTree);
+  const rightSubtree = reconstructBSTFromArray(
+    rootVal,
+    right,
+    preorder,
+    newTree
+  );
+  return new BST(rootVal, leftSubtree, rightSubtree);
+};
