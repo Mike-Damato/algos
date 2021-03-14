bc419622e80b310b33677ae1db60712e95a5c13c
Mike-Damato <damatomike@gmail.com>
2021-03-14 10:59:24 -0400

    Add - 1721 Swapping Nodes In LinkedList Alternate Solution

--- LeetCode/Medium/1721SwappingNodesInLinkedList.js
+++ LeetCode/Medium/1721SwappingNodesInLinkedList.js
@@ -43,3 +43,36 @@ const swapNodes = (head, k) => {
   [arr[frontK].val, arr[endK].val] = [arr[endK].val, arr[frontK].val];
   return head;
 };
+
+//With constant space optimization
+const swapNodes = (head, k) => {
+  let newList = new ListNode(0, head);
+  let p1 = newList;
+  let p2 = newList;
+  let p3 = newList;
+  let length = 0;
+
+  //Get total length to use for calculating K
+  while (p1) {
+    p1 = p1.next;
+    length++;
+  }
+
+  //Front node
+  for (let i = 0; i < k - 1; i++) {
+    p2 = p2.next;
+  }
+
+  //End Node
+  for (let i = 0; i < length - k - 1; i++) {
+    p3 = p3.next;
+  }
+
+  let temp1 = p3.next.val;
+  let temp2 = p2.next.val;
+
+  p2.next.val = temp1;
+  p3.next.val = temp2;
+
+  return head;
+};
