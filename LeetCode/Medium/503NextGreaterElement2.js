278a8877297cc94c6608197c979e2066efa0291a
Mike-Damato <damatomike@gmail.com>
2021-04-18 12:23:23 -0400

    Add - 503 Next Greater Element 2

---
+++ LeetCode/Medium/503NextGreaterElement2.js
@@ -0,0 +1,13 @@
+var nextGreaterElements = function (nums) {
+  const n = nums.length;
+  const stack = [];
+  const ans = new Array(n).fill(-1);
+
+  for (let i = 0; i < 2 * n; i++) {
+    while (stack.length && nums[i % n] > nums[stack[stack.length - 1]]) {
+      ans[stack.pop()] = nums[i % n];
+    }
+    stack.push(i % n);
+  }
+  return ans;
+};
